bd9846ebe22a52b8641ca82447e6da7c4c989783
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-13 22:47:23 -0700

    added media queries for larger screen sizes

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ export default function About() {
    <>
     <div className='wrap-about'>
         <p className='about-text p-3'>I am a full-stack developer out of Phoenix, AZ. Coding has grown to be a passion of mine. I enjoy the constant problem solving and embrace the challenges it brings</p>
+        <br />
+        <p className='about-text p-3'>I hold myself to a high standard in anything that I do. I like to push myself to reach new limits and am not statisfied with anything less out of my work.</p>
         <div className='key-sills text-warning'>
             <p className='text-warning fw-bold'>Key qualities: </p>
             <ul style={{listStyleType: 'none'}}>
@@ -16,6 +18,21 @@ export default function About() {
                 </li>
                 <li>
                    Detail-oriented
+                </li>
+            </ul>
+
+        </div>
+        <div className='key-sills text-warning'>
+            <p className='text-warning fw-bold'>Strong suits: </p>
+            <ul style={{listStyleType: 'none'}}>
+                <li>
+                    React js
+                </li>
+                <li>
+                    Node js / Express js
+                </li>
+                <li>
+                   MongoDB
                 </li>
             </ul>
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,51 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 export default function Nav() {
   const location = useLocation();
   const [showNav, setShowNav] = useState(false);
+  console.log(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
+  const activeLinkStyle = (path) => {
+    return {
+        backgroundColor: location.pathname === path ? 'grey' : ''
+    }
+  }
+
+  const nestedActiveLink = (path) => {
+    return {
+        textDecoration: 'none', 
+        color: location.pathname === path ? '#c9c9c9' : 'white', 
+        display: 'block'
+    }
+  }
+
 
   const toggleNav = () => {
     setShowNav(prevState => !prevState);
   }
 
+  useEffect(() => {
+    window.addEventListener('resize', function () {
+        setWindowWidth(window.innerWidth)
+    })
+  }, [windowWidth])
+
   console.log();
   return (
     <>
-        <nav className="navbar navbar-expand nav-bg mb-5">
-            <div className="container-fluid d-flex flex-column">
-                <button onClick={toggleNav} className='d-flex flex-column cst-nav p-1 align-items-center justify-content-center mb-2'>
+        <nav className="navbar navbar-expand nav-bg mb-5 mt-3">
+            <div className="container-fluid flex-query">
+                <button onClick={toggleNav} className='cst-nav p-1 mb-2'>
                     <span className='border-bottom border-dark border-2 w-100 my-1'></span>
                     <span className='border-bottom border-dark border-2 w-100 my-1'></span>
                     <span className='border-bottom border-dark border-2 w-100 my-1'></span>
                 </button>
-                {showNav && <div className='wrap-nav-options d-flex flex-column w-100'>
-                    <button className="navbar-brand text-white cst-link px-3"><Link to="/portfolio" style={{textDecoration: 'none', color: 'white', display: 'block'}}>Project Catalog</Link></button>
-                    <button className="navbar-brand text-white cst-link px-3"><Link to="/contact" style={{textDecoration: 'none', color: 'white', display: 'block'}}>Contact</Link></button>
+                {(showNav || windowWidth > 644) && <div className='wrap-nav-options w-100'>
+                    <button className="navbar-brand cst-link px-3" style={activeLinkStyle('/')}><Link to="/" style={nestedActiveLink('/')}>About</Link></button>
+                    <button className="navbar-brand cst-link px-3" style={activeLinkStyle('/portfolio')}><Link to="/portfolio" style={nestedActiveLink('/portfolio')}>Project Catalog</Link></button>
+                    <button className="navbar-brand cst-link px-3" style={activeLinkStyle('/contact')}><Link to="/contact" style={nestedActiveLink('/contact')}>Contact</Link></button>
                 </div>}
 
             </div>
