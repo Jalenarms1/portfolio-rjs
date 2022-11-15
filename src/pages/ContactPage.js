db6dbc1fa77c819adb6609d9f5ed837f579f1699
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-15 11:08:29 -0700

    fixed bottom page bug

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -8,8 +8,8 @@ export default function ContactPage() {
             <h1 className="pb-2 text-warning fw-bold">Contact</h1>
         </div>
 
-        <div id="proj-coll" className="wrap-proj-sec d-flex flex-wrap m-auto pb-2">
-            <div className=" cst-wrap d-flex m-auto flex-wrap"> 
+        <div id="proj-coll" className="wrap-proj-sec d-flex flex-wrap m-auto pb-5">
+            <div className="cst-wrap d-flex m-auto flex-wrap"> 
                 <Contact />
 
             </div>
